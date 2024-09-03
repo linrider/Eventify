ff181af20fe729cb3c5d2b88e0eeb80dc61b0d33
Vladyslav Lazin <wladislaw.lazin@gmail.com>
2024-09-03 23:17:08 +0300

    first commit

---
+++ src/main/java/ua/lazin/vladyslav/eventify/domain/User.java
@@ -0,0 +1,46 @@
+package ua.lazin.vladyslav.eventify.domain;
+
+import java.util.Set;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.experimental.FieldDefaults;
+import lombok.AccessLevel;
+import lombok.Data;
+
+@Entity
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE) 
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    long id;
+
+    @Column(name = "user_name")
+    String userName;
+
+    String name;
+
+    String surname;
+
+    String email;
+
+    String password;
+
+    @Column(name = "phone_number")
+    String phoneNumber;
+
+    @Column(name = "is_email_verified")
+    boolean isEmailVerified;
+
+    @Column(name = "is_phone_verified")
+    boolean isPhoneVerified;
+
+    Set<Role> roles;
+
+}
